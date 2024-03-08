d9766e67f0935b78c3c5622437da9e12ba900a46
DiegoCampos20151718 <143902081+DiegoCampos20151718@users.noreply.github.com>
2024-03-07 21:07:39 -0600

    Bootstrap y Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,62 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">All Videogames</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Register</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Login</a>
+          </li>
+        </ul>
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <select class="form-select" aria-label="Default select example">
+            <option selected>All categories</option>
+            <option value="1">Shooter</option>
+            <option value="2">Adventure</option>
+            <option value="3">RPG</option>
+          </select>
+          <select class="form-select" aria-label="Default select example">
+            <option selected>All plataforms</option>
+            <option value="1">Xbox</option>
+            <option value="2">Playstation</option>
+            <option value="3">Polystation</option>
+          </select>
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+    <main class="section">
+      <From />
+    </main>
+  </div>
 </template>
 
+<script setup>
+import From from './components/From.vue'
+</script>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+<style>
+/* Estilos para el espacio debajo del navbar */
+.section {
+  padding-top: 4rem;
+  /* Ajusta este valor según el tamaño del navbar */
 }
 </style>
+
+<script>
+// Importa Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Importa Bootstrap JS (requiere jQuery y Popper.js)
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+</script>./components/Form.vue./components/From.vue
